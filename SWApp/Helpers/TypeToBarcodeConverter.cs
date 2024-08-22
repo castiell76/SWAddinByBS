b183369bf74655bee0dc47839affbd44406900bc
castiell76 <blazejsz1994@gmail.com>
2024-08-22 13:10:26 +0200

    22.08W

---
+++ SWApp/Helpers/TypeToBarcodeConverter.cs
@@ -0,0 +1,35 @@
+﻿using BarcodeStandard;
+using NPOI.SS.UserModel;
+using SkiaSharp;
+using SolidWorks.Interop.sldworks;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+
+namespace SWApp.Helpers
+{
+    public class TypeToBarcodeConverter 
+    {
+        public static byte[] GenerateBarcode(string text)
+        {
+            Barcode barcode = new Barcode();
+            var barcodeImage = barcode.Encode(BarcodeStandard.Type.Code128, text, SKColors.Black, SKColors.White, 300, 100);
+            using (var ms = new MemoryStream())
+            {
+                barcodeImage.Save(ms, image.RawFormat);
+                return ms.ToArray();
+            }
+        }
+    }
+
+}
+    
+
--- SWApp/Models/ExcelFile.cs
+++ SWApp/Models/ExcelFile.cs
@@ -16,6 +16,8 @@ using Wpf.Ui.Controls;
 using NPOI.XWPF.UserModel;
 using NPOI.OpenXmlFormats.Spreadsheet;
 using NPOI.SS.Util;
+using SWApp.Helpers;
+using Aspose.Pdf;
 
 namespace SWApp.Models
 {
@@ -42,14 +44,16 @@ namespace SWApp.Models
         };
 
         public event Action<string, string, ControlAppearance, SymbolIcon> ErrorOccurred;
-        public void CreateWorkBook(DataTable dt, string indexName, string filepath, string assemblyFilepath, string assemblyConfig, string projectName, string size, string assemblyMass)
+        public void CreateWorkBook(DataTable dt, string indexName, string filepath, string assemblyFilepath, string assemblyConfig, string projectName,  string size, string assemblyMass)
         {
             SWObject swObject = new SWObject();
             string modelFilepath;
             string configName;
             byte[] imageBytes;
+            byte[] barcodeBytes;
 
             int pictureIndex;
+            int barcodeIndex;
             int k = 0;
             int i = 0;
             XSSFCreationHelper helper;
@@ -119,7 +123,7 @@ namespace SWApp.Models
             #endregion
             #region row 1
             cellMainIndex = rowMainIndex1.CreateCell(4) as XSSFCell;
-            cellMainIndex.SetCellValue("gabaryty (dług X szer X wys");
+            cellMainIndex.SetCellValue("wymiary gabarytowe");
             cellMainIndex.CellStyle = headerIndexStyle;
 
             cellMainIndex = rowMainIndex1.CreateCell(5) as XSSFCell;
@@ -177,176 +181,196 @@ namespace SWApp.Models
             dataStyle.BorderRight = BorderStyle.Thin;
             dataStyle.BorderTop = BorderStyle.Thin;
             dataStyle.SetFont(dataFont);
+            dataStyle.WrapText = true;
             XSSFRow rowMain = sheet.CreateRow(3) as XSSFRow;
 
 
-            //styling headers
-            foreach (DataColumn column in dt.Columns)
-            {
-                if (column.ColumnName == "status" || column.ColumnName == "createdBy" || column.ColumnName == "checkedBy" || column.ColumnName == "dxfExist" ||
-                    column.ColumnName == "assemblyFilePath" || column.ColumnName == "stepExist" || column.ColumnName == "assemblyConfig")
-                {
+            ////styling headers
+            //foreach (DataColumn column in dt.Columns)
+            //{
+            //    if (column.ColumnName == "status" || column.ColumnName == "createdBy" || column.ColumnName == "checkedBy" || column.ColumnName == "dxfExist" ||
+            //        column.ColumnName == "assemblyFilePath" || column.ColumnName == "stepExist" || column.ColumnName == "assemblyConfig")
+            //    {
 
-                }
-                else
-                {
-                    XSSFCell cellMain = rowMain.CreateCell(k) as XSSFCell;
-                    cellMain.SetCellValue(ColumnsNames[column.ColumnName]);
-                    cellMain.CellStyle = headerStyle;
-                    k++;
-                }
-            }
+            //    }
+            //    else
+            //    {
+            //        XSSFCell cellMain = rowMain.CreateCell(k) as XSSFCell;
+            //        cellMain.SetCellValue(ColumnsNames[column.ColumnName]);
+            //        cellMain.CellStyle = headerStyle;
+            //        k++;
+            //    }
+            //}
 
-            //creating new rows with data
-            for (i = 0; i < dt.Rows.Count; i++)
-            {
-                XSSFRow row = sheet.CreateRow(i + 4) as XSSFRow;
-                for (int j = 0; j < dt.Columns.Count; j++)
-                {
-                    DataColumn dc = dt.Columns[j];
-                    row.Height = 2500;
-                    if (dc.ColumnName == "filepath")
-                    {
-                        XSSFCell cell = row.CreateCell(j) as XSSFCell;
-                        sheet.SetColumnWidth(j, 10000);
+            ////creating new rows with data
+            //for (i = 0; i < dt.Rows.Count; i++)
+            //{
+            //    XSSFRow row = sheet.CreateRow(i + 4) as XSSFRow;
+            //    for (int j = 0; j < dt.Columns.Count; j++)
+            //    {
+            //        DataColumn dc = dt.Columns[j];
+            //        row.Height = 2500;
+            //        if (dc.ColumnName == "filepath")
+            //        {
+            //            XSSFCell cell = row.CreateCell(j) as XSSFCell;
+            //            sheet.SetColumnWidth(j, 10000);
 
-                        configName = dt.Rows[i].Field<string>("configuration");
-                        modelFilepath = dt.Rows[i].Field<string>(j);
-                        imageBytes = swObject.GetBitMap(modelFilepath, configName);
-                        try
-                        {
-                            pictureIndex = workbook.AddPicture(imageBytes, (NPOI.SS.UserModel.PictureType)XSSFWorkbook.PICTURE_TYPE_BMP);
-                            helper = workbook.GetCreationHelper() as XSSFCreationHelper;
-                            drawing = sheet.CreateDrawingPatriarch() as XSSFDrawing;
-                            anchor = helper.CreateClientAnchor() as XSSFClientAnchor;
-                            anchor.Dx1 = 100000;
-                            anchor.Dy1 = 50000;
-                            anchor.Col1 = j;
-                            anchor.Row1 = i + 4;
-                            picture = drawing.CreatePicture(anchor, pictureIndex) as XSSFPicture;
-                            cell.CellStyle = dataStyle;
-                            picture.Resize(1, 1);
+            //            configName = dt.Rows[i].Field<string>("configuration");
+            //            modelFilepath = dt.Rows[i].Field<string>(j);
+            //            imageBytes = swObject.GetBitMap(modelFilepath, configName);
+            //            try
+            //            {
+            //                pictureIndex = workbook.AddPicture(imageBytes, (NPOI.SS.UserModel.PictureType)XSSFWorkbook.PICTURE_TYPE_BMP);
+            //                helper = workbook.GetCreationHelper() as XSSFCreationHelper;
+            //                drawing = sheet.CreateDrawingPatriarch() as XSSFDrawing;
+            //                anchor = helper.CreateClientAnchor() as XSSFClientAnchor;
+            //                anchor.Dx1 = 100000;
+            //                anchor.Dy1 = 50000;
+            //                anchor.Col1 = j;
+            //                anchor.Row1 = i + 4;
+            //                picture = drawing.CreatePicture(anchor, pictureIndex) as XSSFPicture;
+            //                cell.CellStyle = dataStyle;
+            //                picture.Resize(1, 1);
 
 
                        
-                        }
-                        catch (FileNotFoundException)
-                        {
+            //            }
+            //            catch (FileNotFoundException)
+            //            {
 
-                        }
+            //            }
 
 
-                    }
-                    else if (dc.ColumnName == "type")
-                    {
-                        XSSFCell cell = row.CreateCell(j) as XSSFCell;
-                        var type = dt.Rows[i].Field<string>(j);
-                        switch (type)
-                        {
-                            case "part":
-                                cell.SetCellValue("część");
-                                cell.CellStyle = dataStyle;
-                                break;
-                            case "assembly":
-                                cell.SetCellValue("złożenie");
-                                cell.CellStyle = dataStyle;
-                                break;
-                            case "sheet":
-                                cell.SetCellValue("wcięte");
-                                cell.CellStyle = dataStyle;
-                                break;
-                        }
-                    }
+            //        }
+            //        else if (dc.ColumnName == "type")
+            //        {
+            //            XSSFCell cell = row.CreateCell(j) as XSSFCell;
+            //            var type = dt.Rows[i].Field<string>(j);
+            //            switch (type)
+            //            {
+            //                case "part":
+            //                    cell.SetCellValue("część");
+            //                    cell.CellStyle = dataStyle;
+            //                    break;
+            //                case "assembly":
+            //                    cell.SetCellValue("złożenie");
+            //                    cell.CellStyle = dataStyle;
+            //                    break;
+            //                case "sheet":
+            //                    cell.SetCellValue("wcięte");
+            //                    cell.CellStyle = dataStyle;
+            //                    break;
+            //            }
+            //        }
 
-                    else if (dc.ColumnName != "status" && dc.ColumnName != "createdBy" && dc.ColumnName != "checkedBy" && dc.ColumnName != "dxfExist" &&
-                             dc.ColumnName != "assemblyFilePath" && dc.ColumnName != "stepExist" && dc.ColumnName != "assemblyConfig")
-                    {
-                        XSSFCell cell = row.CreateCell(j) as XSSFCell;
-                        cell.SetCellValue(dt.Rows[i].Field<string>(j));
-                        sheet.AutoSizeColumn(j);
-                        cell.CellStyle = dataStyle;
-                    }
+            //        else if (dc.ColumnName != "status" && dc.ColumnName != "createdBy" && dc.ColumnName != "checkedBy" && dc.ColumnName != "dxfExist" &&
+            //                 dc.ColumnName != "assemblyFilePath" && dc.ColumnName != "stepExist" && dc.ColumnName != "assemblyConfig")
+            //        {
+            //            XSSFCell cell = row.CreateCell(j) as XSSFCell;
+            //            cell.SetCellValue(dt.Rows[i].Field<string>(j));
+            //            sheet.SetColumnWidth(0, 5000);
+            //            //sheet.AutoSizeColumn(j);
+            //            cell.CellStyle = dataStyle;
+            //        }
 
-                }
-                row.Height = 2500;
-            }
+            //    }
+            //    row.Height = 2500;
+            //}
 
             //cell and its where need to add assembly bitmap
-            XSSFCell cellAssemblyPic = rowMainIndex0.CreateCell(0) as XSSFCell;
-            headerIndexStyle.VerticalAlignment = NPOI.SS.UserModel.VerticalAlignment.Center;
-            headerIndexStyle.Alignment = NPOI.SS.UserModel.HorizontalAlignment.Center;
-            rowMainIndex0.Height = 1666;
-            rowMainIndex1.Height = 1666;
-            rowMainIndex2.Height = 1666;
-            sheet.SetColumnWidth(0, 10000);
+            //XSSFCell cellAssemblyPic = rowMainIndex0.CreateCell(0) as XSSFCell;
+            //headerIndexStyle.VerticalAlignment = NPOI.SS.UserModel.VerticalAlignment.Center;
+            //headerIndexStyle.Alignment = NPOI.SS.UserModel.HorizontalAlignment.Center;
+            //rowMainIndex0.Height = 1666;
+            //rowMainIndex1.Height = 1666;
+            //rowMainIndex2.Height = 1666;
+            //sheet.SetColumnWidth(0, 10000);
 
-            //adding image of assembly
-            imageBytes = swObject.GetBitMap(assemblyFilepath, assemblyConfig);
-            //data = File.ReadAllBytes(assemblyImageFilepath);
-            pictureIndex = workbook.AddPicture(imageBytes, (NPOI.SS.UserModel.PictureType)XSSFWorkbook.PICTURE_TYPE_BMP);
+            ////adding image of assembly
+            //imageBytes = swObject.GetBitMap(assemblyFilepath, assemblyConfig);
+            //pictureIndex = workbook.AddPicture(imageBytes, (NPOI.SS.UserModel.PictureType)XSSFWorkbook.PICTURE_TYPE_BMP);
+            //helper = workbook.GetCreationHelper() as XSSFCreationHelper;
+            //drawing = sheet.CreateDrawingPatriarch() as XSSFDrawing;
+            //anchor = helper.CreateClientAnchor() as XSSFClientAnchor;
+            //anchor.Dx1 = 50000;
+            //anchor.Dy1 = 25000;
+            //anchor.Col1 = 0;
+            //anchor.Row1 = 0;
+            //picture = drawing.CreatePicture(anchor, pictureIndex) as XSSFPicture;
+            //picture.Resize(2, 3);
+
+            //adding index barcode
+            sheet.AddMergedRegion(new NPOI.SS.Util.CellRangeAddress(1, 1, 8, 9));
+            barcodeBytes = TypeToBarcodeConverter.ConvertStringToBarcode(indexName);
+            barcodeIndex = workbook.AddPicture(barcodeBytes, (NPOI.SS.UserModel.PictureType)XSSFWorkbook.PICTURE_TYPE_BMP);
             helper = workbook.GetCreationHelper() as XSSFCreationHelper;
             drawing = sheet.CreateDrawingPatriarch() as XSSFDrawing;
             anchor = helper.CreateClientAnchor() as XSSFClientAnchor;
-            anchor.Dx1 = 50000;
-            anchor.Dy1 = 25000;
-            anchor.Col1 = 0;
-            anchor.Row1 = 0;
-            picture = drawing.CreatePicture(anchor, pictureIndex) as XSSFPicture;
-            picture.Resize(2, 3);
+            anchor.Dx1 = 100000;
+            anchor.Dy1 = 50000;
+            anchor.Col1 = 8;
+            anchor.Row1 = 1;
+            picture = drawing.CreatePicture(anchor, barcodeIndex) as XSSFPicture;
+            var barcodeCell = rowMainIndex1.CreateCell(8);
+            barcodeCell.CellStyle = dataStyle;
+            picture.Resize(1, 1);
 
+            //resize type column
             sheet.AutoSizeColumn(3);
 
+            
 
-            //format as table
-            XSSFTable table = sheet.CreateTable();
-            CT_Table ctTable = table.GetCTTable();
-            AreaReference myDataRange = new AreaReference(new CellReference(3, 0), new CellReference(i+4, 13));
-            ctTable.@ref = myDataRange.FormatAsString();
-            ctTable.id = 1;
-            ctTable.name = "Table1";
-            ctTable.displayName = "Table1";
-            ctTable.tableStyleInfo = new CT_TableStyleInfo();
-            ctTable.tableStyleInfo.name = "TableStyleMedium2"; // TableStyleMedium2 is one of XSSFBuiltinTableStyle
-            ctTable.tableStyleInfo.showRowStripes = true;
-            ctTable.autoFilter = new CT_AutoFilter(); 
-            ctTable.autoFilter.filterColumn = new List<CT_FilterColumn>(); 
 
-            for(int l = 1; l<15; l++)
-            {
-                ctTable.autoFilter.filterColumn.Add(new CT_FilterColumn()
-                { colId = Convert.ToUInt32(l), showButton = true });
-            }
+            ////format as table
+            //XSSFTable table = sheet.CreateTable();
+            //CT_Table ctTable = table.GetCTTable();
+            //AreaReference myDataRange = new AreaReference(new CellReference(3, 0), new CellReference(i+4, 13));
+            //ctTable.@ref = myDataRange.FormatAsString();
+            //ctTable.id = 1;
+            //ctTable.name = "Table1";
+            //ctTable.displayName = "Table1";
+            //ctTable.tableStyleInfo = new CT_TableStyleInfo();
+            //ctTable.tableStyleInfo.name = "TableStyleMedium2"; // TableStyleMedium2 is one of XSSFBuiltinTableStyle
+            //ctTable.tableStyleInfo.showRowStripes = true;
+            //ctTable.autoFilter = new CT_AutoFilter(); 
+            //ctTable.autoFilter.filterColumn = new List<CT_FilterColumn>(); 
+
+            //for(int l = 1; l<15; l++)
+            //{
+            //    ctTable.autoFilter.filterColumn.Add(new CT_FilterColumn()
+            //    { colId = Convert.ToUInt32(l), showButton = true });
+            //}
 
             
-            ctTable.tableColumns = new CT_TableColumns();
-            ctTable.tableColumns.tableColumn = new List<CT_TableColumn>();
-            ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 1, name = "widok" });
-            ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 2, name = "nazwa" });
-            ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 3, name = "opis" });
-            ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 4, name = "typ" });
-            ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 5, name = "materiał" });
-            ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 6, name = "indeks" });
-            ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 7, name = "grubość [mm]" });
-            ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 8, name = "masa [kg]" });
-            ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 9, name = "powierzchnia [dm2]" });
-            ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 10, name = "ilość farby [kg]" });
-            ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 11, name = "nr rysunku" });
-            ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 12, name = "sztuk na kpl" });
-            ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 13, name = "konfiguracja" });
-            ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 14, name = "uwagi" });
-            //save file
-            try
-            {
+            //ctTable.tableColumns = new CT_TableColumns();
+            //ctTable.tableColumns.tableColumn = new List<CT_TableColumn>();
+            //ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 1, name = "widok" });
+            //ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 2, name = "nazwa" });
+            //ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 3, name = "opis" });
+            //ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 4, name = "typ" });
+            //ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 5, name = "materiał" });
+            //ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 6, name = "indeks" });
+            //ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 7, name = "grubość [mm]" });
+            //ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 8, name = "masa [kg]" });
+            //ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 9, name = "powierzchnia [dm2]" });
+            //ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 10, name = "ilość farby [kg]" });
+            //ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 11, name = "nr rysunku" });
+            //ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 12, name = "sztuk na kpl" });
+            //ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 13, name = "konfiguracja" });
+            //ctTable.tableColumns.tableColumn.Add(new CT_TableColumn() { id = 14, name = "uwagi" });
+            ////save file
+            //try
+            //{
 
-                using (FileStream stream = new FileStream(filepath, FileMode.Create, FileAccess.Write))
-                {
-                    workbook.Write(stream);
-                }
-            }
-            catch (IOException)
-            {
-                ErrorOccurred?.Invoke("Uwaga!", "Wybrano plik tylko do odczytu. Plik nie został zapisany", ControlAppearance.Caution, new SymbolIcon(SymbolRegular.Important24));
-            }
+            //    using (FileStream stream = new FileStream(filepath, FileMode.Create, FileAccess.Write))
+            //    {
+            //        workbook.Write(stream);
+            //    }
+            //}
+            //catch (IOException)
+            //{
+            //    ErrorOccurred?.Invoke("Uwaga!", "Wybrano plik tylko do odczytu. Plik nie został zapisany", ControlAppearance.Caution, new SymbolIcon(SymbolRegular.Important24));
+            //}
 
         }
 
